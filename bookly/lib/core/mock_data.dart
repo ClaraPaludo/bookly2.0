bdf7e746f7776df4ef5a1bd01f76d28f06a8640e
Clara <clara.bpaludo@gmail.com>
2026-07-13 17:05:13 -0300

    Melhora estrutura SQLite e remove copia duplicada do projeto

--- bookly/lib/core/mock_data.dart
+++
@@ -1,6 +0,0 @@
-class MockData {
-  static const int totalBooks = 0;
-  static const int pendingLoans = 0;
-  static const int returnedLoans = 0;
-  static const int lateLoans = 0;
-}
--- bookly/lib/models/friend_model.dart
+++
@@ -1,6 +0,0 @@
-class FriendModel {
-  String name;
-  String email;
-
-  FriendModel({required this.name, required this.email});
-}
